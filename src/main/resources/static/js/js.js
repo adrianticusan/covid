da6f7dc9e368c3283f58f1344d520e3f6d99f09c
unknown <ducajust4u@gmail.com>
2020-04-07 14:30:25 +0300

    added error mesagges design for login modal

--- src/main/resources/static/js/js.js
+++ src/main/resources/static/js/js.js
@@ -16,30 +16,36 @@ $(document).ready(function () {
 
     $(".j-button-login").click(manageLogin);
 
-
     const displayModal = () => {
-        if (window.innerWidth > 1024) { // reset default browser
-            $("body")[0].style.paddingRight = "17px";
-        }
-        $(".j-overlay-modal")[0].style.display = "block";
-        $("body")[0].style.overflow = "hidden";
+        $(".j-overlay-modal").css({"display": "block"});
+        $("body").css({"overflow": "hidden", "padding-right": "17px"});
         setTimeout(() => {
+            //
+            // login modal comes in middle
             $(".holder-login").addClass("j-login-top");
         }, 100);
-        // reset default browser
-        $("body")[0].style.overflow = "auto";
-
     };
+
     const cancelModal = () => {
+        // when overlay is clicked
+        // make login modal to go over top
         $(".holder-login").addClass("j-login-reverse-top");
+        // make modal email to go over down
         $(".holder-forgot-pass").addClass("j-forgot-reverse-bottom ");
-        // cancel effects of modal email messages
-        $(".j-email-mesj").css({"visibility": "hidden"});
+        // make email and login modal disappear, clear value and put css default
+        $(".j-email-mesg").css({"visibility": "hidden"});
+        $(".j-login-mesg").css({"visibility": "hidden"});
         $(".holder-forgot-pass input").css({"border-color": "#35ac39"});
         $(".holder-forgot-pass input").val("");
+        $("#login-user").css({"border-color": "#35ac39"});
+        $("#login-pass").css({"border-color": "#35ac39"});
+        $(".holder-login input").val("");
+
 
         setTimeout(() => {
-            $(".j-overlay-modal")[0].style.display = "none";
+            //
+            // reset all, default email modal, login modal, overlay and body
+            $(".j-overlay-modal").css({"display": "none"})
             $("body").css({"overflow": "auto", "padding-right": "0px"});
             $(".holder-login").removeClass("j-login-reverse-top  j-login-top");
             $(".holder-forgot-pass").removeClass("j-forgot-bottom j-forgot-reverse-bottom ");
@@ -51,7 +57,10 @@ $(document).ready(function () {
     $(".j-overlay-modal")[0].addEventListener("click", cancelModal, false);
 
     $(".j-forgot")[0].addEventListener("click", function () {
+        //
+        // after click on forgot pass make login modal to go overtop
         $(".holder-login").addClass("j-login-reverse-top ");
+        // show the email modal
         $(".holder-forgot-pass").addClass("j-forgot-bottom");
     }, false);
 
@@ -65,20 +74,38 @@ function manageLogin(event) {
     });
 }
 
-$(".j-email-btn")[0].addEventListener('click', function () { // put your email validation
+$(".j-button-login")[0].addEventListener('click', function () {
+    //
+    // put your login validation
+    const username = "user"
+    const pass = "123";
+    const inputUsername = $("#login-user").val();
+    const inputPass = $("#login-pass").val();
+    $(".j-login-mesg ").css({"visibility": "visible"});
+    if (username === inputUsername && pass === inputPass) {
+        $("#login-user").css({"border-color": "#35ac39"})
+        $("#login-pass").css({"border-color": "#35ac39"})
+        $(".j-login-mesg ").css({"visibility": "hidden"});
+    } else {
+        $("#login-user").css({"border-color": "red"})
+        $("#login-pass").css({"border-color": "red"})
+    }
+
+}, false);
+
+$(".j-email-btn")[0].addEventListener('click', function () {
+    //
+    // put your email validation
     const email = "an email";
     const inputValue = $(".holder-forgot-pass input").val();
-    $(".j-email-mesj ").css({"visibility": "visible"});
-
-
+    $(".j-email-mesg ").css({"visibility": "visible"});
     if (email === inputValue) {
-        $(".j-email-mesj ").css({"color": "#35ac39"});
+        $(".j-email-mesg ").css({"color": "#35ac39"});
         $(".holder-forgot-pass input").css({"border-color": "#35ac39"})
-        $(".j-email-mesj ")[0].textContent = "An email has been sent";
+        $(".j-email-mesg ")[0].textContent = "An email has been sent";
     } else {
-        $(".j-email-mesj ").css({"color": "red"})
+        $(".j-email-mesg ").css({"color": "red"})
         $(".holder-forgot-pass input").css({"border-color": "red"})
-        $(".j-email-mesj ")[0].textContent = "The email that you entered does not exists";
-
+        $(".j-email-mesg ")[0].textContent = "The email that you entered does not exists";
     }
 }, false)
