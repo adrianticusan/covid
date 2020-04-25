5f81ccd5cb6852f556b2804071f4a782924dc06c
unknown <ducajust4u@gmail.com>
2020-04-25 11:27:45 +0300

    added support for ie10+

--- src/main/resources/static/js/settings.js
+++ src/main/resources/static/js/settings.js
@@ -1,18 +1,15 @@
+"use strict";
+
 $(document).ready(function () {});
-
-
 var slider = $("#myRange")[0];
 var output = $("#value-slider")[0];
 output.innerHTML = slider.value + " miles";
 // Display the default slider value
-
 // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function () {
-    output.innerHTML = `${
-        this.value
-    } miles`;
-}
 
+slider.oninput = function () {
+    output.innerHTML = "".concat(this.value, " miles");
+};
 
 function initAutocomplete() {
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -22,59 +19,58 @@ function initAutocomplete() {
         },
         zoom: 13,
         mapTypeId: 'roadmap'
-    });
+    }); // Create the search box and link it to the UI element.
 
+    var input = $("#location-input")[0];
+    var searchBox = new google.maps.places.SearchBox(input); // Bias the SearchBox results towards current map's viewport.
 
-    // Create the search box and link it to the UI element.
-    const input = $("#location-input")[0];
-    const searchBox = new google.maps.places.SearchBox(input);
-
-    // Bias the SearchBox results towards current map's viewport.
     map.addListener('bounds_changed', function () {
         searchBox.setBounds(map.getBounds());
     });
     var markers = [];
     // Listen for the event fired when the user selects a prediction and retrieve
     // more details for that place.
+
     var markers = [];
     // Listen for the event fired when the user selects a prediction and retrieve
     // more details for that place.
+
     searchBox.addListener('places_changed', function () {
         var places = searchBox.getPlaces();
 
         if (places.length == 0) {
             return;
-        }
-        // Clear out the old markers.
+        } // Clear out the old markers.
+
+
         markers.forEach(function (marker) {
             marker.setMap(null);
         });
-        markers = [];
+        markers = []; // For each place, get the icon, name and location.
 
-        // For each place, get the icon, name and location.
         var bounds = new google.maps.LatLngBounds();
         places.forEach(function (place) {
             if (! place.geometry) {
                 console.log("Returned place contains no geometry");
                 return;
             }
+
             var icon = {
                 url: place.icon,
                 size: new google.maps.Size(71, 71),
                 origin: new google.maps.Point(0, 0),
                 anchor: new google.maps.Point(17, 34),
                 scaledSize: new google.maps.Size(25, 25)
-            };
-            // Create a marker for each place.
+            }; // Create a marker for each place.
+
             markers.push(new google.maps.Marker({map: map, icon: icon, title: place.name, position: place.geometry.location}));
+
             if (place.geometry.viewport) { // Only geocodes have viewport.
                 bounds.union(place.geometry.viewport);
             } else {
                 bounds.extend(place.geometry.location);
             }
-
         });
         map.fitBounds(bounds);
-
     });
 }
