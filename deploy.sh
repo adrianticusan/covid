2e6cfa29d59ef2856d99df605cc058e3a5aa4a10
Adrian Ticusan <adi.ticusan99@gmail.com>
2020-04-25 21:31:17 +0300

    Deploy script and remove amazon queue.

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -rf build/libs/*
+./gradlew clean assemble
+mv build/libs/*.war build/libs/ROOT.war
+ssh root@covid19helpusa.com 'mkdir -p ~/oldDeploys/$(date +%Y%m%d)/ && cp -f /opt/tomcat/webapps/ROOT.war ~/oldDeploys/$(date +%Y%m%d)/ && rm -rf /opt/tomcat/webapps/ROOT && chown  tomcat:tomcat /opt/tomcat/webapps/ROOT.war'
+scp -r $(pwd)/build/libs/ROOT.war root@covid19helpusa.com:/opt/tomcat/webapps/
+ssh root@covid19helpusa.com 'sudo service tomcat restart'
+ssh root@covid19helpusa.com 'tail -f /opt/tomcat/logs/catalina.out'
--- src/main/java/com/covid19/match/CovidApplication.java
+++ src/main/java/com/covid19/match/CovidApplication.java
@@ -3,6 +3,7 @@ package com.covid19.match;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -17,5 +18,10 @@ public class CovidApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CovidApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CovidApplication.class);
     }
 }
